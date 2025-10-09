1dcf2fb52a2793fa89d4239d1c44ffb0d7192f7f
Lucas Paulo <lp152659@gmail.com>
2025-10-09 20:24:09 -0300

    fix: Resolução de upload de imagem pela rota

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -3,11 +3,12 @@ const userRouter = express.Router();
 
 import { register, login, profile, registerPet } from "../controllers/userController.js";
 import { authGuard } from "../middlewares/authGurard.js";
+import upload from "../middlewares/upload.js";
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
 userRouter.get("/profile", authGuard, profile);
-userRouter.post("/register-pet", authGuard, registerPet);
+userRouter.post("/register-pet", authGuard, upload.single("imagem"), registerPet);
 
 
 export default userRouter;
